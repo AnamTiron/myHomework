4237b1aa34fc4c0ec3147abb6af70e847373981d
Tiron Anamaria <tiron.anam@gmail.com>
2016-07-03 19:56:46 +0300

    Fiind prima aplicatie C#, mi-a dat putin de cap dar cred ca am reusit sa rezolv toate exercitiile. Astept feedback :)

---
+++ Test/MyHomework/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1= new Employee();
+            // employee1.displayInfo();
+            employee1.addNewLeave();
+
+
+        }
+    }
+}
